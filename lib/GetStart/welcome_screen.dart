b0b2a2b9c8b6cc27a080f8da2c3ab33f1a97ddc0
Nischal Khatri <nischal06khatri@gmail.com>
2021-12-21 22:21:03 +0545

    setting up the project

---
+++ lib/GetStart/welcome_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FrontScreen extends StatelessWidget {
+  const FrontScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
