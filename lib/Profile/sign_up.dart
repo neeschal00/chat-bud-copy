20900f66756a397e74d52130d3c02382215dddf7
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-03 01:21:27 +0545

    Adding lootie

--- lib/Profile/sign_up.dart
+++ lib/Profile/sign_up.dart
@@ -1,42 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:lottie/lottie.dart';
 
 class SignUp extends StatelessWidget {
   final formKey = GlobalKey<FormBuilderState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Column(
-            children: [
-              Center(
-                child: Image.asset(
-                  'assets/images/launch_image.png',
-                  fit: BoxFit.contain,
-                  width: 100,
-                  height: 100,
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child: Column(
+              children: [
+                Center(
+                  child: Lottie.asset('assets/lottie/signup.json'),
                 ),
-              ),
-              Container(
-                child: FormBuilder(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      FormBuilderTextField(
-                        name: 'textfield',
-                      ),
-                      ElevatedButton(
-                        child: Text('Submit'),
-                        onPressed: () {},
-                      ),
-                    ],
+                Container(
+                  child: FormBuilder(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        FormBuilderTextField(
+                          name: 'textfield',
+                        ),
+                        ElevatedButton(
+                          child: Text('Submit'),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    onChanged: () => (print(formKey.currentState)),
+                    autovalidateMode: AutovalidateMode.always,
                   ),
-                  onChanged: () => (print(formKey.currentState)),
-                  autovalidateMode: AutovalidateMode.always,
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
