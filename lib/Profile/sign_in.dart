a8b3f8f3283587b5660847f64ad605d85c0033b6
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-03 01:22:33 +0545

    Adjusting the ui with design

--- lib/Profile/sign_in.dart
+++ lib/Profile/sign_in.dart
@@ -1,41 +1,86 @@
+import 'package:chat_bud/Profile/sign_in_controller.dart';
+import 'package:chat_bud/Profile/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
-class SignIn extends StatelessWidget {
-  final formKey = GlobalKey<FormBuilderState>();
+class SignIn extends GetView<SignInController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Container(
           child: Center(
             child: Column(
               children: [
-                Center(
-                  child: Image.asset(
-                    'assets/images/launch_image.png',
-                    fit: BoxFit.contain,
-                    width: 100,
-                    height: 100,
+                Container(
+                  child: Center(
+                    child: Image.asset(
+                      'assets/images/signin.png',
+                      fit: BoxFit.contain,
+                      width: 250,
+                      height: 300,
+                    ),
                   ),
                 ),
                 Container(
+                  child: Center(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
                   child: FormBuilder(
-                    key: formKey,
+                    key: controller.formKey,
                     child: Column(
                       children: [
-                        FormBuilderTextField(
-                          name: 'textfield',
-                          decoration:
-                              InputDecoration(labelText: "Enter Your number"),
+                        Container(
+                          child: FormBuilderTextField(
+                            name: 'textfield',
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Username"),
+                          ),
+                          width: 300,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          child: FormBuilderTextField(
+                            name: 'textfield',
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Password"),
+                          ),
+                          width: 300,
+                        ),
+                        SizedBox(
+                          height: 20,
                         ),
                         ElevatedButton(
                           child: Text('Submit'),
-                          onPressed: () {},
+                          onPressed: () {
+                            if (controller.formKey.currentState!
+                                .saveAndValidate()) {
+                              print(controller.formKey.currentState!.value);
+                            }
+                            Get.to(SignUp());
+                          },
                         ),
                       ],
                     ),
-                    onChanged: () => (print(formKey.currentState)),
+                    onChanged: () => (print(controller.formKey.currentState)),
                     autovalidateMode: AutovalidateMode.always,
                   ),
                 )
