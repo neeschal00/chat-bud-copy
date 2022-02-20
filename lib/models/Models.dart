1fde3c880ef28f39cc55c2c8e1fca7c8fc760643
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-21 00:23:04 +0545

    models updated

--- lib/models/Models.dart
+++ lib/models/Models.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/cupertino.dart';
 
-import 'package:flutter/cupertino.dart';
-
+// User
 class User {
   final String id;
-  final String email;
   final String name;
-  User({
-    this.id,
-    @required this.email,
-    @required this.name,
-  });
+  final String email;
+
+  User({this.id, @required this.name, @required this.email});
 }
 
+// User Data
 class UserData {
   final String id;
   final String email;
   final String name;
-  UserData({
-    this.id,
-    this.email,
-    this.name,
-  });
+
+  UserData(this.id, this.email, this.name);
 }
 
+// Dynamic Data for users
 class ChatUsers {
   String name;
   String messageText;
@@ -36,8 +31,34 @@ class ChatUsers {
   });
 }
 
-class ChatMessage {
-  String messageContent;
-  String messageType;
-  ChatMessage({@required this.messageContent, @required this.messageType});
+// Chat Models
+class ChatUser {
+  String receiverEmail;
+  String senderEmail;
+  String roomID;
+
+  ChatUser(
+      {@required this.receiverEmail,
+      @required this.senderEmail,
+      @required this.roomID});
+}
+
+// Message Model
+class Message {
+  String id;
+  String roomID;
+  String txtMsg;
+  String receiverEmail;
+  String senderEmail;
+  String time;
+  bool sender;
+
+  Message(
+      {this.id,
+      this.roomID,
+      this.txtMsg,
+      this.receiverEmail,
+      this.senderEmail,
+      this.time,
+      this.sender});
 }
