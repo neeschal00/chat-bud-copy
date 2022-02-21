8ae04488fda951a9eac55ccd7d0679900d8fb9ad
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-21 22:32:13 +0545

    work with main view

---
+++ lib/views/Home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:chat_bud/Screens/ChatScreen.dart';
+import 'package:chat_bud/Screens/LoginScreen.dart';
+import 'package:chat_bud/providers/ChatsProvider.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    Provider.of<ChatsNotifierProvider>(context, listen: false).getUserChats();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFF100E20),
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: Text("Chatify"),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 15.0),
+              child: IconButton(
+                  onPressed: () async {
+                    SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+                    prefs.remove("jwt");
+                    prefs.remove("id");
+                    prefs.clear();
+                    Future.delayed(
+                        Duration(seconds: 2),
+                        () => {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => LoginScreen()))
+                            });
+                  },
+                  icon: Icon(Icons.logout)),
+            )
+          ],
+        ),
+        body: ChatScreen());
+  }
+}
