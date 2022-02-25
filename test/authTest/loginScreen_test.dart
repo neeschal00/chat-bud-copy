79d3d93a5035799ac7946101a9b73fcbd736eb2c
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-26 01:41:08 +0545

    Some more test cases

--- test/authTest/loginScreen_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets("input login details", (WidgetTester tester) async {
-    final email = find.byKey(ValueKey('email'));
-    final password = find.byKey(ValueKey('password'));
-  });
-}
