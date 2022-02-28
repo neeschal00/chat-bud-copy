458e5391263440a1c71814f1359fd9de06fa95bb
Nischal Khatri <nischal06khatri@gmail.com>
2022-02-28 15:44:29 +0545

    added api login test

--- test/authTest/login_test.dart
+++ test/authTest/login_test.dart
@@ -2,6 +2,7 @@ import 'package:mockito/mockito.dart';
 import 'package:chat_bud/providers/AuthenticationProvider.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:chat_bud/Model/User.dart';
+import 'package:http/http.dart' as http;
 
 class MockUser extends Mock implements User {}
 
@@ -10,14 +11,11 @@ class MockAuth extends Mock implements AuthenticationProvider {}
 void main() {
   final MockAuth mockAuth = MockAuth();
 
-  setUp(() {
-    // setup code
-  });
-  tearDown(() {
-    // tear down code
-  });
   test('test sign in', () async {
-    // arrange
+    final url = Uri.parse("http://localhost:3000/login");
+    final res = await http
+        .post(url, body: {'"email": "test@gmail.com","password": "123456"'});
+    expect(res.statusCode, 500);
 
     // act
     // assert
